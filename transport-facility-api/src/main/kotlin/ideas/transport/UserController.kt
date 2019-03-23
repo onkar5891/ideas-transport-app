d5c101d19bfebcca760408e2602c727c9353c483
Onkar Kamatkar <onkar.kamatkar@ideas.com>
2019-03-23 19:08:00 +0530

    Users CRUD impl

--- transport-facility-api/src/main/kotlin/ideas/transport/UserController.kt
+++ transport-facility-api/src/main/kotlin/ideas/transport/UserController.kt
@@ -1,17 +1,134 @@
 package ideas.transport
 
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.jdbc.support.GeneratedKeyHolder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.*
+import java.sql.ResultSet
 
 @RestController
 @RequestMapping("/user")
-class UserController {
-    @RequestMapping(value = ["/hello"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun hello(): User {
-        return User(1, "user2",980,null, "Male", "Pune", 411027)
+class UserController(val service: UserService) {
+    @RequestMapping(
+            value = ["/save"],
+            method = [RequestMethod.POST],
+            produces = [MediaType.APPLICATION_JSON_VALUE],
+            consumes = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun save(@RequestBody user: User): User? {
+        val userId = service.save(user)
+        return findById(userId)
+    }
+
+    @RequestMapping(value = ["/find/{userId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findById(@PathVariable("userId") userId: Int): User? = service.findById(userId)
+
+    @RequestMapping(value = ["/findAll"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun list(): List<User> = service.findAll()
+}
+
+@Service
+class UserService(val namedParameterJdbcTemplate: NamedParameterJdbcTemplate) {
+    val logger: Logger = LogManager.getLogger(UserService::class.qualifiedName)
+
+    fun findAll(): List<User> {
+        return namedParameterJdbcTemplate.query(FIND_ALL_QUERY, allColumnsRowMapper())
+    }
+
+    fun findById(userId: Int): User? {
+        val parameterSource = MapSqlParameterSource(
+                mapOf(Pair("userId", userId))
+        )
+
+        val result = namedParameterJdbcTemplate.query(FIND_BY_ID_QUERY, parameterSource, allColumnsRowMapper())
+        if (!result.isEmpty()) {
+            return result[0]
+        }
+        return null
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    fun save(user: User): Int = if (user.userId == null) create(user) else update(user)
+
+    private fun create(user: User): Int {
+        logger.info("Creating new User..")
+
+        val parameterSource = MapSqlParameterSource(
+                mapOf(
+                        "name" to user.name,
+                        "contactNo" to user.contactNo,
+                        "alternateContactNo" to user.alternateContactNo,
+                        "gender" to user.gender,
+                        "address" to user.address,
+                        "pinCode" to user.pinCode
+                )
+        )
+        val keyHolder = GeneratedKeyHolder()
+
+        namedParameterJdbcTemplate.update(INSERT_QUERY, parameterSource, keyHolder)
+        return keyHolder.key.toInt()
+    }
+
+    private fun update(user: User): Int {
+        logger.info("Updating User: ${user.userId}")
+
+        val parameterSource = MapSqlParameterSource(
+                mapOf(
+                        "userId" to user.userId,
+                        "name" to user.name,
+                        "contactNo" to user.contactNo,
+                        "alternateContactNo" to user.alternateContactNo,
+                        "gender" to user.gender,
+                        "address" to user.address,
+                        "pinCode" to user.pinCode
+                )
+        )
+
+        namedParameterJdbcTemplate.update(UPDATE_QUERY, parameterSource)
+        return user.userId!!
+    }
+
+    private fun allColumnsRowMapper(): (ResultSet, Int) -> User = { rs, _ ->
+        User(
+                rs.getInt("user_id"),
+                rs.getString("name"),
+                rs.getInt("contact_no"),
+                rs.getInt("alternate_contact_no"),
+                rs.getString("gender"),
+                rs.getString("address"),
+                rs.getInt("pincode")
+        )
     }
 }
 
-data class User(val userId: Int, val name: String, val contactNo : Int, val alternateContactNo : Int?,val gender:String , val address : String, val pincode:Int)
+data class User(
+        var userId: Int?,
+        var name: String,
+        var contactNo: Int,
+        var alternateContactNo: Int?,
+        var gender: String,
+        var address: String,
+        var pinCode: Int
+)
+
+const val INSERT_QUERY =
+        "INSERT INTO Users " +
+                "(name, contact_no, alternate_contact_no, gender, address, pinCode) " +
+                "VALUES " +
+                "(:name, :contactNo, :alternateContactNo, :gender, :address, :pinCode)"
+const val UPDATE_QUERY =
+        "UPDATE Users " +
+                "SET name = :name, contact_no = :contactNo, alternate_contact_no = :alternateContactNo, gender = :gender, address = :address, pincode = :pinCode " +
+                "WHERE user_id = :userId"
+const val FIND_ALL_QUERY =
+        "SELECT * FROM " +
+                "Users"
+const val FIND_BY_ID_QUERY =
+        "SELECT * FROM Users " +
+                "WHERE user_id = :userId"
---
+++ transport-facility-api/src/main/kotlin/ideas/transport/config/TransportAppConfig.kt
@@ -0,0 +1,43 @@
+package ideas.transport.config
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import javax.sql.DataSource
+
+@Configuration
+class TransportAppConfig {
+    @Value("\${spring.datasource.url}")
+    lateinit var url: String
+    @Value("\${spring.datasource.username}")
+    lateinit var username: String
+    @Value("\${spring.datasource.password}")
+    lateinit var password: String
+
+    @Bean
+    fun namedParameterJdbcTemplate(): NamedParameterJdbcTemplate {
+        return NamedParameterJdbcTemplate(dataSource())
+    }
+
+    @Bean
+    fun dataSource(): DataSource {
+        val dataSource = SQLServerDataSource()
+        dataSource.url = url
+        dataSource.user = username
+        dataSource.setPassword(password)
+        return dataSource
+    }
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper()
+                .enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
+                .enable(DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY)
+                .registerKotlinModule()
+    }
+}
